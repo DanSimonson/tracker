b33a4d97040c0729cdab1af2623f699e53a6f861
daniel simonson <dansimonson@mariposaweb.net>
2021-12-07 22:39:39 -0600

    first commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import data from "./data.js";
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(5000, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
