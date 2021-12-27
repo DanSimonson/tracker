1c522a93f6d35f4e99d18568d90bfe56c1d30abd
daniel simonson <dansimonson@mariposaweb.net>
2021-12-27 04:46:02 -0600

    started styling home page

--- frontend/src/Views/Home.js
+++ frontend/src/Views/Home.js
@@ -4,11 +4,12 @@ import LoginForm from "../Components/Login/LoginForm";
 import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { getUsers } from "../Redux/usersSlice";
+import "./Home.scss";
 
 function Home() {
   const dispatch = useDispatch();
   const newState = useSelector((state) => state);
-  console.log("newState: ", newState);
+  //console.log("newState: ", newState);
   const { users, status, isAdmin } = useSelector((state) => state.users);
   const [prod, setProd] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -50,6 +51,7 @@ function Home() {
     }
     //inputRef.current.focus();
   };
+  const submitForm = () => {};
 
   return (
     <>
@@ -61,7 +63,7 @@ function Home() {
           marginTop: "2rem",
         }}
       >
-        Users
+        Timer
       </div>
       <div>
         {status === "loading" ? (
@@ -78,6 +80,35 @@ function Home() {
           //   ))}
           // </h1>
           <>
+            <h1>Pomodro ⏲ with 🛎</h1>
+            <figure className="clock">
+              <div className="mins">0</div>
+              <div>:</div>
+              <div className="secs">00</div>
+              <audio src="http://soundbible.com/mp3/service-bell_daniel_simion.mp3"></audio>
+              <svg className="progress-ring" height="120" width="120">
+                <circle
+                  className="progress-ring__circle"
+                  stroke-width="8"
+                  fill="transparent"
+                  r="50"
+                  cx="60"
+                  cy="60"
+                />
+              </svg>
+            </figure>
+            <div className="btn-group">
+              <button className="start">start focus</button>
+              <button className="reset">reset</button>
+              <button className="pause">pause</button>
+            </div>
+            <form onSubmit={submitForm}>
+              <label for="focusTime">Focus Time</label>
+              <input type="number" value="1" id="focusTime" />
+              <label for="breakTime">Break Time</label>
+              <input type="number" value="1" id="breakTime" />
+              <button type="submit">Save settings</button>
+            </form>
             <section>
               <button onClick={startTimer}>Start</button>
               <button onClick={stopTimer}>Stop</button>
