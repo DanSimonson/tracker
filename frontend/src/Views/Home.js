73bbe3c8fbda601fd6c78210218100e19d10255c
daniel simonson <dansimonson@mariposaweb.net>
2021-12-27 02:46:00 -0600

    added basic countdown start, stop, reset logic

--- frontend/src/Views/Home.js
+++ frontend/src/Views/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Products from "../Components/Products";
 import LoginForm from "../Components/Login/LoginForm";
 import axios from "axios";
@@ -13,10 +13,43 @@ function Home() {
   const [prod, setProd] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [randomInput, setRandomInput] = useState("");
+  const [seconds, setSeconds] = useState(20);
+  const renders = useRef(0);
+  const inputRef = useRef();
+  const timerId = useRef();
 
   useEffect(() => {
     dispatch(getUsers());
   }, []);
+
+  const handleChange = (e) => {
+    setRandomInput(e.target.value);
+    renders.current++;
+  };
+
+  const startTimer = () => {
+    timerId.current = setInterval(() => {
+      renders.current++;
+      setSeconds((prev) => prev - 1);
+    }, 1000);
+    //inputRef.current.focus();
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerId.current);
+    timerId.current = 0;
+    //inputRef.current.focus();
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    if (seconds) {
+      renders.current++;
+      setSeconds(0);
+    }
+    //inputRef.current.focus();
+  };
 
   return (
     <>
@@ -36,14 +69,24 @@ function Home() {
         ) : error ? (
           <h1>Error...</h1>
         ) : status === "success" ? (
-          <h1>
-            {users.map((user) => (
-              <ul key={user.id} style={{ listStyle: "none" }}>
-                <li>user: {user.name}</li>
-                <li>email: {user.email}</li>
-              </ul>
-            ))}
-          </h1>
+          // <h1>
+          //   {users.map((user) => (
+          //     <ul key={user.id} style={{ listStyle: "none" }}>
+          //       <li>user: {user.name}</li>
+          //       <li>email: {user.email}</li>
+          //     </ul>
+          //   ))}
+          // </h1>
+          <>
+            <section>
+              <button onClick={startTimer}>Start</button>
+              <button onClick={stopTimer}>Stop</button>
+              <button onClick={resetTimer}>Reset</button>
+            </section>
+            <br />
+            <br />
+            <p>Seconds: {seconds}</p>
+          </>
         ) : null}
       </div>
     </>
