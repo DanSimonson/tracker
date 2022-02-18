e9bbf60ba342f148ead6c48ddb52b8b164a8828e
daniel simonson <dansimonson@mariposaweb.net>
2022-02-18 05:23:24 -0600

    removed code in db.js

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -4,14 +4,14 @@ const connectDB = async () => {
   try {
     //console.log("process.env.MONGO_URI:", process.env.MONGO_URI);
     //let uri = process.env.MONGO_URI;
-    let tempUri = process.env.REACT_APP_MONGO_URI;
+    //let tempUri = process.env.REACT_APP_MONGO_URI;
     //console.log("process.env.MONGO_URI: ", process.env.MONGO_URI);
     //consolole.log(typeof process.env.MONGO_URI);
-    console.log(
-      "process.env.REACT_APP_MONGO_URI: ",
-      process.env.REACT_APP_MONGO_URI
-    );
-    console.log(typeof process.env.REACT_APP_MONGO_URI);
+    // console.log(
+    //   "process.env.REACT_APP_MONGO_URI: ",
+    //   process.env.REACT_APP_MONGO_URI
+    // );
+    //console.log(typeof process.env.REACT_APP_MONGO_URI);
     const connection = await mongoose.connect(
       "mongodb+srv://dan123:dan123@resfulapi.7d1qe.mongodb.net/tracker?retryWrites=true&w=majority",
       {
