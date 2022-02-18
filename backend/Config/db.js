8bcec263b2d6ea0a711aad26026e6c3a77aaa771
daniel simonson <dansimonson@mariposaweb.net>
2022-02-18 07:12:09 -0600

    change

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
+//process.env.MONGO_URI
+//mongodb+srv://dan123:dan123@resfulapi.7d1qe.mongodb.net/tracker?retryWrites=true&w=majority
 const connectDB = async () => {
   try {
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      //useCreateIndex: true,
-    });
+    const connection = await mongoose.connect(
+      "mongodb+srv://dan123:dan123@resfulapi.7d1qe.mongodb.net/tracker?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        //useCreateIndex: true,
+      }
+    );
     console.log(`MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
     console.error(`Mongo connect error: ${error.message}`);
