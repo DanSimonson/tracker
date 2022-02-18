cf81400a225ca7f216b1f2ae846d55662b1390d8
daniel simonson <dansimonson@mariposaweb.net>
2022-02-18 02:53:13 -0600

    publishing to heroku

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,14 @@ app.use("/api/timer", timerRouter);
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
