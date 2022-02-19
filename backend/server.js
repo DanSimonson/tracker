a385690ba7bfd42546ea62bc1c99a62e370b5a11
daniel simonson <dansimonson@mariposaweb.net>
2022-02-19 17:02:45 -0600

    config for heroku

--- backend/server.js
+++ backend/server.js
@@ -18,20 +18,32 @@ app.use(express.urlencoded({ extended: true }));
 //routes
 app.use("/api/users", userRouter);
 app.use("/api/timer", timerRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 // app.get("/api/products", (req, res) => {
 //   res.send(data.products);
 // });
 
 const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
+}
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
-});
+// app.use(express.static(path.join(__dirname, "/frontend/build")));
+// app.get("*", (req, res) =>
+//   res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+// );
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(5000, () => {
