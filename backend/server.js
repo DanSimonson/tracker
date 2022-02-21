7ce174a0581087a57f29b307b9ac963418e81f01
daniel simonson <dansimonson@mariposaweb.net>
2022-02-21 17:51:11 -0600

    commit

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,6 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000;
 console.log("server.js port: ", port);
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`serve at http://localhost:${port}`);
 });
